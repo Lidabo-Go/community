bac4a1609969e38f3ab8342c917de8a4580ae90f
lidabo <1636799006@qq.com>
2022-07-26 20:06:19 +0800

    首次提交

---
+++ src/test/java/com/nowcoder/community/test/MapperTest.java
@@ -0,0 +1,35 @@
+package com.nowcoder.community.test;
+
+import com.nowcoder.community.CommunityApplication;
+import com.nowcoder.community.dao.DiscussPostMapper;
+import com.nowcoder.community.entity.DiscussPost;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+/**
+ *
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class MapperTest {
+    @Autowired
+    private DiscussPostMapper discussPostMapper;
+
+    @Test
+    public void testSelectPosts() {
+        List<DiscussPost> discussPosts = discussPostMapper.selectDiscussPosts(0, 0, 10);
+        for (DiscussPost discussPost : discussPosts) {
+            System.out.println(discussPost);
+        }
+        int rows = discussPostMapper.selectDiscussPostRows(101);
+        System.out.println(rows);
+    }
+}
